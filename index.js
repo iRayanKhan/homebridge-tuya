42d49123f39aac10cd4d650158752a63525801d5
Gio Palacino <gpalacino@staywell.com>
2018-10-29 10:09:24 -0500

    Added dimmer accessory

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const TuyaDiscover = require('./lib/discovery');
+const DimmerAccessory = require('./lib/dimmer');
 const GenericAccessory = require('./lib/generic');
 
 class TuyaPlatform {
@@ -23,13 +24,13 @@ class TuyaPlatform {
     });
 
     // When a new device is found, add it to Homebridge
-    this.discovery.on('device-new', device => {
+    this.discovery.on('device-new', (device) => {
       this.log.info('New Device Online: %s', device.id);
       this.addAccessory(device);
     });
 
     // If a device is unreachable, remove it from Homebridge
-    this.discovery.on('device-offline', device => {
+    this.discovery.on('device-offline', (device) => {
       this.log.info('Device Offline: %s', device.id);
 
       const uuid = this.api.hap.uuid.generate(device.id + device.name);
@@ -45,7 +46,12 @@ class TuyaPlatform {
 
   // Function invoked when homebridge tries to restore cached accessory
   configureAccessory(accessory) {
-    this.log.info('Configuring cached accessory: [%s] %s %s', accessory.displayName, accessory.context.deviceId, accessory.UUID);
+    this.log.info(
+      'Configuring cached accessory: [%s] %s %s',
+      accessory.displayName,
+      accessory.context.deviceId,
+      accessory.UUID
+    );
     this.log.debug('%j', accessory);
     this.homebridgeAccessories.set(accessory.UUID, accessory);
   }
@@ -57,8 +63,18 @@ class TuyaPlatform {
     const uuid = this.api.hap.uuid.generate(device.id + device.name);
     const homebridgeAccessory = this.homebridgeAccessories.get(uuid);
 
-    // Construct new generic accessory
-    const deviceAccessory = new GenericAccessory(this, homebridgeAccessory, device);
+    // Construct new accessory
+    let deviceAccessory;
+    const deviceType = device.type || 'generic';
+    switch (deviceType) {
+      case 'dimmer':
+        deviceAccessory = new DimmerAccessory(this, homebridgeAccessory, device);
+        break;
+      case 'generic':
+      default:
+        deviceAccessory = new GenericAccessory(this, homebridgeAccessory, device);
+        break;
+    }
 
     // Add to global map
     this.homebridgeAccessories.set(uuid, deviceAccessory.homebridgeAccessory);
@@ -72,10 +88,12 @@ class TuyaPlatform {
     this.log.info('Removing: %s', homebridgeAccessory.displayName);
 
     this.homebridgeAccessories.delete(homebridgeAccessory.deviceId);
-    this.api.unregisterPlatformAccessories('homebridge-tuya', 'TuyaPlatform', [homebridgeAccessory]);
+    this.api.unregisterPlatformAccessories('homebridge-tuya', 'TuyaPlatform', [
+      homebridgeAccessory,
+    ]);
   }
 }
 
-module.exports = function (homebridge) {
+module.exports = function(homebridge) {
   homebridge.registerPlatform('homebridge-tuya', 'TuyaPlatform', TuyaPlatform, true);
 };
