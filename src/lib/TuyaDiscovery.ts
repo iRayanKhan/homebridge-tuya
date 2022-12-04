e105655b79ff4d03240280114704eb85acac181e
Long Zheng <long.zheng@gmail.com>
2022-12-04 21:30:20 +1100

    Convert TuyaDiscovery to TypeScript

--- src/lib/TuyaDiscovery.ts
+++ src/lib/TuyaDiscovery.ts
@@ -1,10 +1,15 @@
-const dgram = require('dgram');
-const crypto = require('crypto');
-const EventEmitter = require('events');
+import dgram from 'dgram';
+import crypto from 'crypto';
+import EventEmitter from 'events';
 
 const UDP_KEY = Buffer.from('6c1ec8e2bb9bb59ab50b0daf649b410a', 'hex');
 
 class TuyaDiscovery extends EventEmitter {
+    private discovered: Map<string, any>; // TODO fix type
+    private limitedIds: string[];
+    private _servers: { [port: number]: dgram.Socket };
+    private _running: boolean;
+
     constructor() {
         super();
 
@@ -14,7 +19,7 @@ class TuyaDiscovery extends EventEmitter {
         this._running = false;
     }
 
-    start(props) {
+    start(props: { clear: boolean; ids: string[] }) {
         const opts = props || {};
 
         if (opts.clear) {
@@ -22,8 +27,7 @@ class TuyaDiscovery extends EventEmitter {
             this.discovered.clear();
         }
 
-        this.limitedIds.splice(0);
-        if (Array.isArray(opts.ids)) [].push.apply(this.limitedIds, opts.ids);
+        if (Array.isArray(opts.ids)) this.limitedIds = opts.ids;
 
         this._running = true;
         this._start(6666);
@@ -52,10 +56,13 @@ class TuyaDiscovery extends EventEmitter {
         return this;
     }
 
-    _start(port) {
+    _start(port: number) {
         this._stop(port);
 
-        const server = this._servers[port] = dgram.createSocket({type: 'udp4', reuseAddr: true});
+        const server = (this._servers[port] = dgram.createSocket({
+            type: 'udp4',
+            reuseAddr: true,
+        }));
         server.on('error', this._onDgramError.bind(this, port));
         server.on('close', this._onDgramClose.bind(this, port));
         server.on('message', this._onDgramMessage.bind(this, port));
@@ -65,19 +72,21 @@ class TuyaDiscovery extends EventEmitter {
         });
     }
 
-    _stop(port) {
+    _stop(port: number) {
         if (this._servers[port]) {
             this._servers[port].removeAllListeners();
             this._servers[port].close();
-            this._servers[port] = null;
+            delete this._servers[port];
         }
     }
 
-    _onDgramError(port, err) {
+    _onDgramError(port: number, err: NodeJS.ErrnoException) {
         this._stop(port);
 
         if (err && err.code === 'EADDRINUSE') {
-            console.warn(`[TuyaDiscovery] Port ${port} is in use. Will retry in 15 seconds.`);
+            console.warn(
+                `[TuyaDiscovery] Port ${port} is in use. Will retry in 15 seconds.`
+            );
 
             setTimeout(() => {
                 this._start(port);
@@ -87,30 +96,42 @@ class TuyaDiscovery extends EventEmitter {
         }
     }
 
-    _onDgramClose(port) {
+    _onDgramClose(port: number) {
         this._stop(port);
 
-        console.info(`[TuyaDiscovery] Port ${port} closed.${this._running ? ' Restarting...' : ''}`);
+        console.info(
+            `[TuyaDiscovery] Port ${port} closed.${
+                this._running ? ' Restarting...' : ''
+            }`
+        );
         if (this._running)
             setTimeout(() => {
                 this._start(port);
             }, 1000);
     }
 
-    _onDgramMessage(port, msg, info) {
+    _onDgramMessage(port: number, msg: Buffer, info: dgram.RemoteInfo) {
         const len = msg.length;
-      //  console.log(`[TuyaDiscovery] UDP from ${info.address}:${port} 0x${msg.readUInt32BE(0).toString(16).padStart(8, '0')}...0x${msg.readUInt32BE(len - 4).toString(16).padStart(8, '0')}`);
-        if (len < 16 ||
+        //  console.log(`[TuyaDiscovery] UDP from ${info.address}:${port} 0x${msg.readUInt32BE(0).toString(16).padStart(8, '0')}...0x${msg.readUInt32BE(len - 4).toString(16).padStart(8, '0')}`);
+        if (
+            len < 16 ||
             msg.readUInt32BE(0) !== 0x000055aa ||
             msg.readUInt32BE(len - 4) !== 0x0000aa55
         ) {
-            console.log(`[TuyaDiscovery] ERROR: UDP from ${info.address}:${port}`, msg.toString('hex'));
+            console.log(
+                `[TuyaDiscovery] ERROR: UDP from ${info.address}:${port}`,
+                msg.toString('hex')
+            );
             return;
         }
 
         const size = msg.readUInt32BE(12);
         if (len - size < 8) {
-            console.log(`[TuyaDiscovery] ERROR: UDP from ${info.address}:${port} size ${len - size}`);
+            console.log(
+                `[TuyaDiscovery] ERROR: UDP from ${info.address}:${port} size ${
+                    len - size
+                }`
+            );
             return;
         }
 
@@ -120,8 +141,12 @@ class TuyaDiscovery extends EventEmitter {
         let decryptedMsg;
         if (port === 6667) {
             try {
-                const decipher = crypto.createDecipheriv('aes-128-ecb', UDP_KEY, '');
-                decryptedMsg = decipher.update(cleanMsg, 'utf8', 'utf8');
+                const decipher = crypto.createDecipheriv(
+                    'aes-128-ecb',
+                    UDP_KEY,
+                    ''
+                );
+                decryptedMsg = decipher.update(cleanMsg, undefined, 'utf8');
                 decryptedMsg += decipher.final('utf8');
             } catch (ex) {}
         }
@@ -131,14 +156,24 @@ class TuyaDiscovery extends EventEmitter {
         try {
             const result = JSON.parse(decryptedMsg);
             if (result && result.gwId && result.ip) this._onDiscover(result);
-            else console.log(`[TuyaDiscovery] ERROR: UDP from ${info.address}:${port} decrypted`, cleanMsg.toString('hex'));
+            else
+                console.log(
+                    `[TuyaDiscovery] ERROR: UDP from ${info.address}:${port} decrypted`,
+                    cleanMsg.toString('hex')
+                );
         } catch (ex) {
-            console.error(`[TuyaDiscovery] Failed to parse discovery response on port ${port}: ${decryptedMsg}`);
-            console.error(`[TuyaDiscovery] Failed to parse discovery raw message on port ${port}: ${msg.toString('hex')}`);
+            console.error(
+                `[TuyaDiscovery] Failed to parse discovery response on port ${port}: ${decryptedMsg}`
+            );
+            console.error(
+                `[TuyaDiscovery] Failed to parse discovery raw message on port ${port}: ${msg.toString(
+                    'hex'
+                )}`
+            );
         }
     }
 
-    _onDiscover(data) {
+    _onDiscover(data: any) {
         if (this.discovered.has(data.gwId)) return;
 
         data.id = data.gwId;
@@ -148,10 +183,11 @@ class TuyaDiscovery extends EventEmitter {
 
         this.emit('discover', data);
 
-        if (this.limitedIds.length &&
+        if (
+            this.limitedIds.length &&
             this.limitedIds.includes(data.id) && // Just to avoid checking the rest unnecessarily
             this.limitedIds.length <= this.discovered.size &&
-            this.limitedIds.every(id => this.discovered.has(id))
+            this.limitedIds.every((id) => this.discovered.has(id))
         ) {
             process.nextTick(() => {
                 this.end();
